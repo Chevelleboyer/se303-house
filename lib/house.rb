263e1f5d029e6cb5d070b6159684d90ecdce538c
Chevelle Boyer <chevelleboyer@gmail.com>
2021-04-15 10:30:32 -0700

    Add a third conditional check for verse 3

--- lib/house.rb
+++ lib/house.rb
@@ -5,6 +5,8 @@ class House
 			"This is the house that Jack built.\n"
 		elsif number == 2
 			"This is the malt that lay in the house that Jack built.\n"
+		elsif number == 3
+			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
