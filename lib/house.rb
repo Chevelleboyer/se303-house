28cd152cd3345f21310cc0584f0f653e762b052d
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-27 19:55:50 -0700

    Pass phrase number in case 2

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,7 @@ class House
 		when 1
 			"This is the house that Jack built.\n"
 		when 2
-			"This is#{phrase} the house that Jack built.\n"
+			"This is#{phrase(number)} the house that Jack built.\n"
 		when 3
 			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		when 4
