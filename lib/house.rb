852f06ffaee9ba4a885922ecfabf04f22c21c280
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-27 20:06:41 -0700

    Call phrase in case 5 to eliminate difference between it and else

--- lib/house.rb
+++ lib/house.rb
@@ -3,7 +3,7 @@ class House
 	def line(number)
 		case number
 		when 5
-			"This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is#{phrase(number)} the house that Jack built.\n"
 		when 6
 			"This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 7
@@ -32,7 +32,7 @@ class House
 		when 4
 			" the cat that killed the rat that ate the malt that lay in"
 		when 5
-			"  the dog that worried the cat that killed the rat that ate the malt that lay in"
+			" the dog that worried the cat that killed the rat that ate the malt that lay in"
 		else
 			""
 		end
