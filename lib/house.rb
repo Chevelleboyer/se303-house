100641914dea2d8c7a53df6b103c53f6bb20a477
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:32:32 -0700

    Add difference in case 10 to DATA

--- lib/house.rb
+++ lib/house.rb
@@ -11,7 +11,7 @@ class House
 	# 	when 2
 	# 		DATA[-number].join("")
 
-	DATA = [" the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", ""]
+	DATA = [" the rooster that crowed in the morn that woke", " the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", ""]
 
 	def line(number)
 		"This is#{phrase(number)} the house that Jack built.\n"
