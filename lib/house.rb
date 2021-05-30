a741f195e558d4c63722c42776aa46981cff33f5
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:17:54 -0700

    Remove hardcoded phrase 5 from case

--- lib/house.rb
+++ lib/house.rb
@@ -23,7 +23,6 @@ class House
 			""
 		when 5
 			DATA[-number..-1].join("")
-			" the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 6
 			" the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 7
