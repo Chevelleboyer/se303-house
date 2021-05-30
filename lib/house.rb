079c07e9f3539e88cc1b0ea191bfdf021c3a4f4f
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-30 09:15:04 -0700

    Add empty strings to VERBS as placeholders, allow phrase to get both phrase and verb independently

--- lib/house.rb
+++ lib/house.rb
@@ -1,20 +1,19 @@
 class House
 
-	DATA = [" the horse and the hound and the horn that belonged to", " the farmer sowing his corn that kept", " the rooster that crowed in the morn that woke", " the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", " the house that Jack built."]
-	VERBS = [" belonged to"]
+	DATA = [" the horse and the hound and the horn that", " the farmer sowing his corn that kept", " the rooster that crowed in the morn that woke", " the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", " the house that Jack built."]
+	VERBS = [" belonged to", "", "", "", "", "", "", "", "", "", "", ""]
 
 	def line(number)
 		"This is#{phrase(number)}\n"
 	end
 
 	def phrase(number=nil)
-		self.class::DATA[-number..-1].join("")
+		number.downto(1).collect { |i| self.class::DATA[-i]+self.class::VERBS[-i] }.join("")
 	end
 
 	def recite
 		(1..12).each.collect { |n| line(n) }.join("\n")
 	end
-
 end
 
 class Pirate < House
