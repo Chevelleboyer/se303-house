926277a55d8019780afb624cd24706112d65c368
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-30 08:46:45 -0700

    Move phrase 1 into DATA to remove the empty string placeholder

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,9 @@
 class House
 
-	DATA = [" the horse and the hound and the horn that belonged to", " the farmer sowing his corn that kept", " the rooster that crowed in the morn that woke", " the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", ""]
+	DATA = [" the horse and the hound and the horn that belonged to", " the farmer sowing his corn that kept", " the rooster that crowed in the morn that woke", " the priest all shaven and shorn that married", " the man all tattered and torn that kissed", " the maiden all forlorn that milked", " the cow with the crumpled horn that tossed", " the dog that worried", " the cat that killed", " the rat that ate", " the malt that lay in", " the house that Jack built."]
 
 	def line(number)
-		"This is#{phrase(number)} the house that Jack built.\n"
+		"This is#{phrase(number)}\n"
 	end
 
 	def phrase(number=nil)
@@ -18,7 +18,7 @@ end
 
 class Pirate < House
 	def line(number)
-		"Thar be#{phrase(number)} the house that Jack built.\n"
+		"Thar be#{phrase(number)}\n"
 	end
 end
 
