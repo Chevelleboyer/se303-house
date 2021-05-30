592f8dd43cee820c139f0c1e8431f229fab51532
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:13:22 -0700

    Combine case 2 and 3 into the else branch since they are now the same

--- lib/house.rb
+++ lib/house.rb
@@ -21,10 +21,6 @@ class House
 		case number
 		when 1
 			""
-		when 2
-			DATA[-number..-1].join("")
-		when 3
-			DATA[-number..-1].join("")
 		when 4
 			" the cat that killed the rat that ate the malt that lay in"
 		when 5
@@ -44,7 +40,7 @@ class House
 		when 12
 			" the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		else
-			""
+			DATA[-number..-1].join("")
 		end
 	end
 
