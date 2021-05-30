ecbeb86ec3469c3c23d05b3fdef29b0f0c615d1d
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-30 10:07:48 -0700

    Remove empty string placeholders

--- lib/house.rb
+++ lib/house.rb
@@ -1,14 +1,14 @@
 class House
 
-	DATA = [" the horse and the hound and the horn that", " the farmer sowing his corn that", " the rooster that crowed in the morn that", " the priest all shaven and shorn that", " the man all tattered and torn that", " the maiden all forlorn that", " the cow with the crumpled horn that", " the dog that", " the cat that", " the rat that", " the malt that", ""]
-	VERBS = [" belonged to", " kept", " woke", " married", " kissed", " milked", " tossed", " worried", " killed", " ate", " lay in", ""]
+	DATA = [" the horse and the hound and the horn that", " the farmer sowing his corn that", " the rooster that crowed in the morn that", " the priest all shaven and shorn that", " the man all tattered and torn that", " the maiden all forlorn that", " the cow with the crumpled horn that", " the dog that", " the cat that", " the rat that", " the malt that"]
+	VERBS = [" belonged to", " kept", " woke", " married", " kissed", " milked", " tossed", " worried", " killed", " ate", " lay in"]
 
 	def line(number)
 		"This is#{phrase(number)} the house that Jack built.\n"
 	end
 
 	def phrase(number=nil)
-		number.downto(1).collect { |i| self.class::DATA[-i]+self.class::VERBS[-i] }.join("")
+		(number-1).downto(1).collect { |i| self.class::DATA[-i]+self.class::VERBS[-i] }.join("")
 	end
 
 	def recite
