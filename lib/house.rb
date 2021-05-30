4dfc8d59bdd4c665b4c59f8acf0b0c87768a8635
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:35:21 -0700

    Remove case 11 as it is now the same as else

--- lib/house.rb
+++ lib/house.rb
@@ -21,8 +21,6 @@ class House
 		case number
 		when 1
 			""
-		when 11
-			DATA[-number..-1].join("")
 		when 12
 			" the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		else
