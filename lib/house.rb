92c0b85d9705ca3d66edaba8ee4cac5b171dc302
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:07:06 -0700

    Check syntax for getting phrase 3 from DATA

--- lib/house.rb
+++ lib/house.rb
@@ -22,6 +22,7 @@ class House
 		when 2
 			DATA[-number..-1].join("")
 		when 3
+			DATA[-number..-1].join("")
 			" the rat that ate the malt that lay in"
 		when 4
 			" the cat that killed the rat that ate the malt that lay in"
