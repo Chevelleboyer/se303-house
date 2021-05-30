75936d4271e9850ece08cf4817a82fb7a3b0422d
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:29:09 -0700

    Remove case 8 as it is now the same as else

--- lib/house.rb
+++ lib/house.rb
@@ -21,8 +21,6 @@ class House
 		case number
 		when 1
 			""
-		when 8
-			DATA[-number..-1].join("")
 		when 9
 			" the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 10
