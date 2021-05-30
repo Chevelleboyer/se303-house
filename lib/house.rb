de175f6206d9d2f00dcc5d679c006eef8f2a6d78
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:31:10 -0700

    Test syntax for getting phrase 9 from DATA

--- lib/house.rb
+++ lib/house.rb
@@ -22,6 +22,7 @@ class House
 		when 1
 			""
 		when 9
+			DATA[-number..-1].join("")
 			" the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 10
 			" the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
