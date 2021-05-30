8d00f209c7e9cf36a065dee371eecb8d16c62af1
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-29 19:27:43 -0700

    Remove hardcoded phrase 8 from case

--- lib/house.rb
+++ lib/house.rb
@@ -23,7 +23,6 @@ class House
 			""
 		when 8
 			DATA[-number..-1].join("")
-			" the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 9
 			" the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
 		when 10
